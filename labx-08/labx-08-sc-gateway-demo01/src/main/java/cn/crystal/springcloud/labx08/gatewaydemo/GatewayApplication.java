bfb4e33db193332027fb7a1a9967b918ded37b5d
QiLiangXie <xieqiliang@inspur.com>
2020-05-11 10:23:21 +0800

    init

---
+++ labx-08/labx-08-sc-gateway-demo01/src/main/java/cn/crystal/springcloud/labx08/gatewaydemo/GatewayApplication.java
@@ -0,0 +1,14 @@
+package cn.crystal.springcloud.labx08.gatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xieqiliang
+ */
+@SpringBootApplication
+public class GatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
